08efa9614246ce2515fc0f38cad14545871eef43
pine2104 <ychanc2104@gmail.com>
2021-04-10 12:47:11 +0800

    #1-2 use return to break loop

--- 1_TwoSum.py
+++ 1_TwoSum.py
@@ -10,7 +10,7 @@ class Solution:
                 x[nums[i]] = i
             value = target - nums[i]
             if value in x.keys() and i != x.get(value):
-                return [x.get(value), i]
+                return [x.get(value), i] ## directly exit loop while using return
 
     def twoSum_2(self, nums, target):
         n = len(nums)
@@ -20,9 +20,9 @@ class Solution:
             for j in range(n-1-i):
                 if nums[i] + nums[j+i+1] == target:
                     output = [i, i+j+1]
-                    break
+                    return output
             i += 1
-        return output
+
 
 nums = [3,2,4,1,9,7]
 target = 11
