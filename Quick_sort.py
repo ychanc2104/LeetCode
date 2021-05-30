1123c3a48741eaa10659e139ee16f90bc66f673e
pine2104 <ychanc2104@gmail.com>
2021-05-30 16:53:03 +0800

    Add quick sort

---
+++ Quick_sort.py
@@ -0,0 +1,15 @@
+def quick_sort(data):
+    if len(data) <= 1:
+        return data
+    pivot = data[0]
+    data.remove(pivot)
+    L, R = [], []
+    for x in data:
+        if x < pivot:
+            L += [x]
+        elif x > pivot:
+            R += [x]
+    return quick_sort(L) + [pivot] + quick_sort(R)
+
+data = [38,14, 57, 59, 52, 19, 1, 2]
+data_sort = quick_sort(data)
