9966b6217d99b0ecd46b9e4e1787317ad518d064
pine <ychanc2104@gmail.com>
2022-04-26 18:37:58 +0800

    find max depth of binary tree using dfs

--- MaximumDepthofBinaryTree.py
+++ MaximumDepthofBinaryTree.py
@@ -1,48 +1,69 @@
 # Definition for a binary tree node.
 class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
+    def __init__(self, val=None):
         self.val = val
-        self.left = left
-        self.right = right
-    def __repr__(self):
-        return str(self.val)
+        self.left = None
+        self.right = None
+
+class BinarySearchTree(TreeNode):
     def insert(self, val):
-        if val < self.val:
-            if self.left==None:
-                self.left = TreeNode(val)
+        if self.val!=None:
+            if val <= self.val:
+                if self.left==None:
+                    self.left = BinarySearchTree(val)
+                else:
+                    self.left.insert(val)
             else:
-                self.left.insert(val)
+                if self.right==None:
+                    self.right = BinarySearchTree(val)
+                else:
+                    self.right.insert(val)
         else:
-            if self.right==None:
-                self.right = TreeNode(val)
-            else:
-                self.right.insert(val)
-    def print_tree(self):
+            self.val = val
+
+    def __repr__(self):
+        return f"node value is {self.val}"
+
+    def print_tree_dfs(self):
+        print(self.val)
         if self.left!=None:
-            self.left.print_tree()
-        else:
-            print(self.val)
+            self.left.print_tree_dfs()
         if self.right!=None:
-            self.right.print_tree()
+            self.right.print_tree_dfs()
+    def print_tree_bfs(self):
+        print("print all node by BFS")
+        queue = [self]
+        while queue!=[]:
+            # print(temp)
+            temp = queue.pop(0)
+            print(temp.val)
+            if temp.left!=None:
+                queue.append(temp.left)
+            if temp.right!=None:
+                queue.append(temp.right)
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxDepth(self, root) -> int:
+        depth = 0
+        if root == None:
+            return depth
         else:
-            print(self.val)
+            return max(self.maxDepth(root.left), self.maxDepth(root.right))+1
 
 
 
+bst = BinarySearchTree()
+data = [10,9,5,15,20,12]
 
-# def build_binary_tree(data):
-#     btree = TreeNode()
-#     for i,d in enumerate(data):
-#         if i==0:
-#             btree.val = d
-#         elif i<2**i:
-#             btree.left = TreeNode(d)
+for d in data:
+    bst.insert(d)
 
 
-btree = TreeNode(3)
-btree.insert(9)
-btree.insert(20)
-# btree.insert(None)
-
-data = [3,9,20,None,None,15,7]
-
+max_depth = Solution().maxDepth(bst)
