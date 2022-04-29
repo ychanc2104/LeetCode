d4db3b78f373643841e005e0d06c0a39b432cbf7
pine <ychanc2104@gmail.com>
2022-04-29 18:28:34 +0800

    postorder traversal DFS

---
+++ BinaryTreePostorderTraversal (DFS).py
@@ -0,0 +1,41 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    ## iterative method
+    def postorderTraversal(self, root):
+        stack = [root]
+        ans = []
+        while stack:
+            node = stack.pop()
+            if node:
+                ans.append(node.val)
+                stack.append(node.left)
+                stack.append(node.right)
+        return ans[::-1]
+    ## iterative method
+    def postorderTraversal2(self, root):
+        stack = [(root, False)]
+        ans = []
+        while stack:
+            node, status = stack.pop()
+            if node:
+                if status:
+                    ans.append(node.val)
+                else:
+                    stack.append((node, True))
+                    stack.append((node.right, False))
+                    stack.append((node.left, False))
+        return ans
+
+    ## recursive method
+    def postorderTraversal3(self, root):
+        ans = []
+        if root:
+            ans.extend(self.postorderTraversal(root.left))
+            ans.extend(self.postorderTraversal(root.right))
+            ans.append(root.val)
+        return ans
