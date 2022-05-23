770f60c1f7542018c54a608ac83776d14a26aee8
pine <ychanc2104@gmail.com>
2022-05-23 19:35:52 +0800

    Longest Consecutive Sequence

---
+++ Longest Consecutive Sequence.py
@@ -0,0 +1,36 @@
+# https://leetcode.com/problems/longest-consecutive-sequence/
+
+
+# brute force, TC: O(n^3)
+def longestConsecutive(nums) -> int:
+    res = 0
+    # brute force
+    # O(n)
+    for i in range(len(nums)):
+        num = nums[i]
+        ans = 1
+        # O(n^2), O(n) for num+1 in nums and O(n) for while loop
+        while num + 1 in nums:
+            ans += 1
+            num += 1
+        res = max(res, ans)
+    return res
+
+# TC: O(n)
+def longestConsecutive2(nums) -> int:
+    res = 0
+    nums_set = set(nums)  # small to big
+    for num in nums_set:
+
+        # num is sub-array of an answer, skip
+        if num - 1 in nums_set:
+            continue
+        ans = 1
+        while num + 1 in nums_set:
+            ans += 1
+            num += 1
+        res = max(ans, res)
+    return res
+
+nums = [100,4,200,1,3,2,0,6,5,7,8]
+res = longestConsecutive(nums)
