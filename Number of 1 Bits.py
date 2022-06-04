e1a10d712007a1769f3c878de5be05f8b8ab8209
pine2104 <ychanc2104@gmail.com>
2022-06-05 01:14:14 +0800

    Number of 1 Bits

---
+++ Number of 1 Bits.py
@@ -0,0 +1,22 @@
+# https://leetcode.com/problems/number-of-1-bits/
+
+
+# first thought
+def hammingWeight(n: int) -> int:
+    res = 0
+    ## end with n==0
+    while n > 0:
+        # count odd num
+        if n % 2 == 1:
+            res += 1
+        n = n >> 1
+
+    return res
+
+def hammingWeight2(n: int) -> int:
+    ans = 0
+    while n:
+        # n&(n-1) will remove the rightmost
+        n = n&(n-1)
+        ans += 1
+    return ans
