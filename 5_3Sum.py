c1105d11f31ce92a4121379a241798c6b7c929e1
pine2104 <ychanc2104@gmail.com>
2022-06-19 17:38:40 +0800

    add O(n^2) solution

--- 5_3Sum.py
+++ 5_3Sum.py
@@ -1,7 +1,6 @@
 ## two pointer method
 
-
-def three_sum(nums): ## time complexicity O(n^2)
+def threeSum(nums): ## time complexicity O(n^2)
     nums.sort()
     n = len(nums)
     result = []
@@ -29,6 +28,55 @@ def three_sum(nums): ## time complexicity O(n^2)
                     k -= 1
     return result
 
+# O(n^2)
+def threeSum2(nums):
+    res = set()
+    for i in range(len(nums)):
+        x = nums[i]
+        memo = {}
+        for j in range(i + 1, len(nums)):
+            y = nums[j]
+            if y not in memo:
+                memo[-x - y] = (i, j)
+            elif y in memo and j not in memo[y]:
+                res.add(tuple(sorted((nums[memo[y][0]], nums[memo[y][1]], y))))
+    return res
+
+# O(n^2)
+def threeSum2(nums):
+    ## sort beforehand
+    nums.sort()
+    res = set()
+    for i in range(len(nums)):
+        x = nums[i]
+        memo = {}
+        for j in range(i + 1, len(nums)):
+            y = nums[j]
+            if y not in memo:
+                memo[-x - y] = (i, j)
+            elif y in memo and j not in memo[y]:
+                res.add((nums[memo[y][0]], nums[memo[y][1]], y))
+    return res
+
+# O(n^2), faster
+def threeSum3(nums):
+    ## sort beforehand
+    nums.sort()
+    res = set()
+    for i in range(len(nums)):
+        if i >= 1 and nums[i] == nums[i - 1]:
+            # do not repeat
+            continue
+        x = nums[i]
+        memo = {}
+        for j in range(i + 1, len(nums)):
+            y = nums[j]
+            if y not in memo:
+                memo[-x - y] = (i, j)
+            elif y in memo and j not in memo[y]:
+                res.add((nums[memo[y][0]], nums[memo[y][1]], y))
+    return res
+
 nums = [1,0,-1]
 nums = [-1,0,1,2,-1,-4]
 nums = [-1,0,1,2,-1,-4,-2,-3,3,0,4]
@@ -36,4 +84,5 @@ nums = [-4,-2,-2,-2,0,1,2,2,2,3,3,4,4,6,6]
 nums = [34,55,79,28,46,33,2,48,31,-3,84,71,52,-3,93,15,21,-43,57,-6,86,56,94,74,83,-14,28,-66,46,-49,62,-11,43,65,77,12,47,61,26,1,13,29,55,-82,76,26,15,-29,36,-29,10,-70,69,17,49]
 nums = [-2,0,1,1,2]
 
-result = three_sum(nums)
+result = threeSum(nums)
+
