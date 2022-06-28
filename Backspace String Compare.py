6bd770725f0fd5cda3752d7a3a442435394b0564
YCC <ychanc2104@gmail.com>
2022-06-29 00:08:25 +0800

    add stack and two pointers solutions

--- Backspace String Compare.py
+++ Backspace String Compare.py
@@ -1,5 +1,6 @@
 # https://leetcode.com/problems/backspace-string-compare/
 # https://leetcode.com/problems/backspace-string-compare/discuss/135603/JavaC%2B%2BPython-O(N)-time-and-O(1)-space
+# https://leetcode.com/problems/backspace-string-compare/discuss/145786/Python-tm
 
 from functools import reduce
 
@@ -23,6 +24,50 @@ def backspaceCompare(s: str, t: str) -> bool:
 def backspaceCompare2(s: str, t: str) -> bool:
     function = lambda res, l: res + l if l != '#' else res[:-1]
     return reduce(function, s, '') == reduce(function, t, '')
+
+# use stack, TC:O(n), SC:O(n)
+def backspaceCompare3(s: str, t: str) -> bool:
+    def clean(s):
+        stack = []
+        s = list(s)
+        for l in s:
+            if stack and l == '#':
+                stack.pop()
+            elif l != '#':
+                stack.append(l)
+        return stack
+    return clean(s) == clean(t)
+
+# use two pointers, TC:O(n), SC:O(1)
+def backspaceCompare4(s: str, t: str) -> bool:
+    ## important to get character excluding #
+    def get_char(seq, i):
+        if i < 0:
+            return '', i
+        skip = 0
+        ## get character of s and skip #
+        while seq[i] == '#' or skip > 0:
+            if seq[i] == '#':
+                skip += 1
+            else:
+                skip -= 1
+            i -= 1
+            if i < 0:
+                return '', i
+        return seq[i], i - 1
+
+
+    skip_s, skip_t = 0, 0
+    i_s, i_t = len(s) - 1, len(t) - 1
+    while i_s >= 0 or i_t >= 0:
+        ## get character of s and skip #
+        char_s, i_s = get_char(s, i_s)
+        ## get character of s and skip #
+        char_t, i_t = get_char(t, i_t)
+        ## check
+        if char_s != char_t:
+            return False
+    return True
 
 
 s = "ab##"
