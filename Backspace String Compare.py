c8b38b1069b3031abe38f94449cadcb96975815e
pine <ychanc2104@gmail.com>
2022-06-28 19:05:21 +0800

    add simple solution

--- Backspace String Compare.py
+++ Backspace String Compare.py
@@ -1,7 +1,7 @@
 # https://leetcode.com/problems/backspace-string-compare/
 # https://leetcode.com/problems/backspace-string-compare/discuss/135603/JavaC%2B%2BPython-O(N)-time-and-O(1)-space
 
-
+from functools import reduce
 
 # first thought, TC:O(n), SC:O(n)
 def backspaceCompare(s: str, t: str) -> bool:
@@ -18,3 +18,15 @@ def backspaceCompare(s: str, t: str) -> bool:
         return res
 
     return remove(s) == remove(t)
+
+# first thought, TC:O(n), SC:O(n)
+def backspaceCompare2(s: str, t: str) -> bool:
+    function = lambda res, l: res + l if l != '#' else res[:-1]
+    return reduce(function, s, '') == reduce(function, t, '')
+
+
+s = "ab##"
+t = "c#d#"
+
+res1 = backspaceCompare(s, t)
+res2 = backspaceCompare2(s, t)
