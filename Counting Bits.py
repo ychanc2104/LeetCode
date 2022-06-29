c94a84fa89adb36311ebead3eda6e78a8c70b63b
YCC <ychanc2104@gmail.com>
2022-06-29 23:44:51 +0800

    add a new dp solution

--- Counting Bits.py
+++ Counting Bits.py
@@ -26,3 +26,18 @@ def countBits2(n: int):
         #print(i, i>>1)
         res.append(res[i>>1]+(i&1))
     return res
+
+
+
+def countBits3(n: int):
+    dp = [0]
+    power = 0
+    # 0, 1, 2, 3, 4, 5, 6(110), 7(111), 8(1000), 9(1001)
+    for i in range(1,n+1):
+        if i>2**power:
+            power += 1
+        if i==2**power:
+            dp.append(1)
+        elif i<2**power:
+            dp.append(dp[2**(power-1)] + dp[i-2**(power-1)])
+    return dp
