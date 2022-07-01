3a6471c3324a3c44a9f0f03f622b7d37fc37ad91
YCC <ychanc2104@gmail.com>
2022-07-02 01:13:37 +0800

    add a clean solution

--- Longest Substring Without Repeating Characters.py
+++ Longest Substring Without Repeating Characters.py
@@ -2,13 +2,14 @@
 # https://leetcode.com/problems/longest-substring-without-repeating-characters/discuss/347818/Python3%3A-sliding-window-O(N)-with-explanation
 import collections
 
-# first thought, sliding window, TC:O(n), SC:O(n)
+# first thought, sliding window, TC:O(2n)~O(n), SC:O(m)
 def lengthOfLongestSubstring(s: str) -> int:
     # dvdf, (0,1), (1,2), (1,3)
     if len(s) <= 1: return len(s)
     L, R = 0, 1
     counter = collections.Counter(s[L])
     res = 0
+    # total 2N runs
     while R < len(s):
         counter[s[R]] += 1
         # make window valid
@@ -19,3 +20,17 @@ def lengthOfLongestSubstring(s: str) -> int:
         R += 1
     return res
 
+# directly skip to desire L pos, sliding window, TC:O(n), SC:O(m), m is character set
+def lengthOfLongestSubstring2(s: str) -> int:
+    L = 0
+    memo = {}  # store the current index
+    res = 0
+    for R in range(len(s)):
+        if s[R] in memo:
+            # skip but do not shift backward
+            # ex: tmmzuxt
+            #     L L   R
+            L = max(memo[s[R]] + 1, L)
+        memo[s[R]] = R
+        res = max(res, R - L + 1)
+    return res
