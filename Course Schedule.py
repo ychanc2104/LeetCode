7b23294f5f64233592a6a1446bff29b9e9b9bed3
YCC <ychanc2104@gmail.com>
2022-07-06 23:37:58 +0800

    add a dfs solution

--- Course Schedule.py
+++ Course Schedule.py
@@ -1,8 +1,8 @@
 # https://leetcode.com/problems/course-schedule/
 
+import collections
 
-
-## dfs, recursive method, TC: O(V+E), vertex and edges
+## dfs, recursive method, TC: O(V+E), vertex and edges, SC:O(V+E)
 def canFinish(numCourses: int, prerequisites) -> bool:
     course_pre = {i: [] for i in range(numCourses)}
     for course, pre in prerequisites:
@@ -32,8 +32,39 @@ def canFinish(numCourses: int, prerequisites) -> bool:
 
     return True
 
+## dfs, recursive method, TC: O(V+E), vertex and edges, SC:O(V+E)
+def canFinish2(numCourses: int, prerequisites) -> bool:
+    memo = collections.defaultdict(list)
+    for x in prerequisites:
+        memo[x[0]].append(x[1])
+    # print(memo)
+    visit = set()
+    # 1 => 0
+    #   <=
+    def dfs(course):
+        # print(course, visit)
+        if course in visit:
+            return False
+        if course not in memo:
+            return True
+        pre_list = memo[course]
+        visit.add(course)
+        for c in pre_list:
+            if not dfs(c):
+                return False
+        visit.remove(course)
+        ## mark finished, very important to reduce time complexity
+        del memo[course]
+        return True
+    for i in range(numCourses):
+        if not dfs(i):
+            return False
+    return True
+
+
 numCourses = 7
 prerequisites = [[1,0],[0,3],[0,2],[3,2],[2,5],[4,5],[5,6],[2,4]]
 
 res = canFinish(numCourses, prerequisites)
 
+res2 = canFinish2(numCourses, prerequisites)
