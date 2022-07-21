e8d4fe08ffb6294f1178e0caf8b439d8fa06e304
pine <ychanc2104@gmail.com>
2022-07-21 14:41:46 +0800

    add O(N) solution

--- Gas Station.py
+++ Gas Station.py
@@ -1,5 +1,5 @@
 # https://leetcode.com/problems/gas-station/
-
+# https://leetcode.com/problems/gas-station/discuss/860347/Python-simple-and-very-short-explained-solution-O(n)-O(1)-faster-than-98
 
 # first thought, TC:O(n^2),
 def canCompleteCircuit(gas, cost) -> int:
@@ -19,9 +19,22 @@ def canCompleteCircuit(gas, cost) -> int:
             return i
     return -1
 
+# TC:O(N), SC:O(1)
+def canCompleteCircuit2(gas, cost) -> int:
+    # unique ans, return gas station index which can finish traversing
+    # if not, return -1
+    substract = [gas[i] - cost[i] for i in range(len(gas))]
+    if sum(substract) < 0:
+        return -1
 
-# def canCompleteCircuit2(gas, cost) -> int:
-
+    curr_sum = 0
+    res = 0
+    for i in range(len(gas)):
+        curr_sum += gas[i] - cost[i]
+        if curr_sum < 0:
+            curr_sum = 0
+            res = i + 1
+    return res
 
 
 gas = [1,2,3,4,3]
