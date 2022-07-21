18693ec428f530497c9be904b3c8c1b96b58dbea
YCC <ychanc2104@gmail.com>
2022-07-22 00:07:01 +0800

    add a solution

--- Next Permutation.py
+++ Next Permutation.py
@@ -2,39 +2,62 @@
 # https://leetcode.com/problems/next-permutation/discuss/14054/Python-solution-with-comments.
 # https://www.nayuki.io/page/next-lexicographical-permutation-algorithm
 
-# def nextPermutation(nums) -> None:
-#     """
-#     Do not return anything, modify nums in-place instead.
-#     """
-#     i_nonin = len(nums)
-#     for i in range(len(nums)-2, -1, -1):
-#         if nums[i] < nums[i+1]:
-#             i_nonin = i
-#             break
-#     if i_nonin==len(nums):
-#         nums.reverse()
-#         return
-#
-#     # diff = nums[i_nonin+1] - nums[i_nonin]
-#     # i_swap = len(nums)-1
-#     j = len(nums) - 1
-#     while j >= 0 and nums[i_nonin] > nums[j]:
-#         j -= 1
-#     i_swap = j
-#     # for j in range(i_nonin+1, len(nums)):
-#     #     if nums[i_nonin] > nums[j]:
-#     #         break
-#     # i_swap = j - 1
-#     print(i_nonin, i_swap)
-#     nums[i_nonin], nums[i_swap] = nums[i_swap], nums[i_nonin]
-#     # reverse
-#     nums[i_nonin+1:] = nums[i_nonin+1:][::-1]
+# TC:O(N), SC:O(N)
+def nextPermutation(nums) -> None:
+    """
+    Do not return anything, modify nums in-place instead.
+    """
+    i_nonin = len(nums)
+    for i in range(len(nums)-2, -1, -1):
+        if nums[i] < nums[i+1]:
+            i_nonin = i
+            break
+    if i_nonin==len(nums):
+        nums.reverse()
+        return
+    j = len(nums) - 1
+    while j >= 0 and nums[i_nonin] >= nums[j]:
+        j -= 1
+    i_swap = j
+    print(i_nonin, i_swap)
+    nums[i_nonin], nums[i_swap] = nums[i_swap], nums[i_nonin]
+    # reverse, take O(N) space
+    nums[i_nonin+1:] = nums[i_nonin+1:][::-1]
 
+# TC:O(N), SC:O(1), better method to reverse
 def nextPermutation2(nums) -> None:
     """
     Do not return anything, modify nums in-place instead.
     """
+    i_nonin = len(nums)
+    for i in range(len(nums) - 2, -1, -1):
+        if nums[i] < nums[i + 1]:
+            i_nonin = i
+            break
+    if i_nonin == len(nums):
+        nums.reverse()
+        return
+    j = len(nums) - 1
+    while j >= 0 and nums[i_nonin] >= nums[j]:
+        j -= 1
+    i_swap = j
+    # print(i_nonin, i_swap)
+    nums[i_nonin], nums[i_swap] = nums[i_swap], nums[i_nonin]
+    # reverse, SC:O(N)
+    # nums[i_nonin+1:] = nums[i_nonin+1:][::-1]
+    # reverse, two-pointer
+    L, R = i_nonin + 1, len(nums) - 1
+    while L < R:
+        nums[L], nums[R] = nums[R], nums[L]
+        L += 1
+        R -= 1
+
+def nextPermutation3(nums) -> None:
+    """
+    Do not return anything, modify nums in-place instead.
+    """
     i = j = len(nums)-1
+    # find first non-decreasing index
     while i > 0 and nums[i-1] >= nums[i]:
         i -= 1
     if i == 0:   # nums are in descending order
@@ -50,7 +73,7 @@ def nextPermutation2(nums) -> None:
         nums[l], nums[r] = nums[r], nums[l]
         l +=1 ; r -= 1
 
-# nums = [0,1,2,5,3,3,0]
+nums = [0,1,2,5,3,3,0]
 # nums = [2,3,1,3,3]
 nums = [1,5,1]
 
