04b350f5560a2faf394a5c0be2054de2f6363e45
YCC <ychanc2104@gmail.com>
2022-08-19 23:40:12 +0800

    fix iterative and add recursive method

--- Pow(x, n).py
+++ Pow(x, n).py
@@ -1,4 +1,6 @@
 # https://leetcode.com/problems/powx-n/
+# https://leetcode.com/problems/powx-n/discuss/19560/Shortest-Python-Guaranteed
+
 
 # TC:O(N), SC:O(1)
 def myPow(x: float, n: int) -> float:
@@ -12,17 +14,27 @@ def myPow(x: float, n: int) -> float:
         res *= x
     return res
 
-# fast power, TC:O(logN), SC:O(1)
+# iterative fast power, TC:O(logN), SC:O(1)
 def myPow2(x: float, n: int) -> float:
-    if n==0: return 1
     if n < 0:
-        x = 1/x
+        x = 1 / x
         n = -n
-    # x^8, (n, res)=>(8,x^2),(4,x^$),(2,x^8),(1,end)
-    res = x
-    while n > 1:
-        if n % 2 == 1:
+    # 2^10, (n,x,res)=>(10,2^2,1),(5,2^4,2^2),(2,2^8,2^2),(1,2^16,2^10), (0,end)
+    res = 1
+    while n > 0:
+        if n & 1:
             res *= x
-        res = res*res
-        n = n//2
+        x *= x
+        n = n // 2
     return res
+
+# recursive fast power, TC:O(logN), SC:O(logN)
+def myPow3(x: float, n: int) -> float:
+    if n < 0:
+        return myPow3(1 / x, -n)
+    elif n == 0:
+        return 1
+    elif n & 1:
+        return x * myPow3(x * x, n >> 1)
+    else:
+        return myPow3(x * x, n >> 1)
