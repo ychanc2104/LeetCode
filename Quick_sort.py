e93659e82d1dada98c66a551e5ea967c2cc8cb56
YCC <ychanc2104@gmail.com>
2022-08-22 23:52:05 +0800

    add in-place quick sort

--- Quick_sort.py
+++ Quick_sort.py
@@ -12,7 +12,30 @@ def quick_sort(data):
             R += [x]
     return quick_sort(L) + [pivot] + quick_sort(R)
 
+# in-place, TC:O(nlogn), SC:O(logn)
+def quick_sort2(data):
+    # move smaller value to L
+    def partition(L, R):
+        pivot = data[R]
+        for i in range(L, R):
+            if data[i] <= pivot:
+                data[i], data[L] = data[L], data[i]
+                L += 1
+        data[L], data[R] = data[R], data[L]
+        return L
+    def helper(L, R):
+        if L>=R: return
+        pos = partition(L, R)
+        helper(L, pos-1)
+        helper(pos+1, R)
+
+    helper(0, len(data)-1)
+
+
+
 if __name__ == '__main__':
 
     data = [38,14, 57, 59, 52, 19, 1, 2, 2, 19]
-    data_sort = quick_sort(data)
+    data_sort = quick_sort(data.copy())
+
+    quick_sort2(data)
