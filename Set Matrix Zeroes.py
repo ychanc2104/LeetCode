d725446028fe199a799de99901f42ea563a1cc0b
pine <ychanc2104@gmail.com>
2022-08-24 15:41:42 +0800

    add SC is O(m+n) and O(1) solutions

--- Set Matrix Zeroes.py
+++ Set Matrix Zeroes.py
@@ -54,3 +54,64 @@ def setZeroes2(matrix):
     # final to check
     if extra_store:
         matrix[0] = [0] * m
+
+# TC:O(M*N), SC:O(M+N)
+def setZeroes3(matrix):
+    """
+    Do not return anything, modify matrix in-place instead.
+    """
+    # store zero position in rows and cols
+    n = len(matrix)
+    m = len(matrix[0])
+    rows = [False] * n
+    cols = [False] * m
+    for i in range(n):
+        for j in range(m):
+            if matrix[i][j] == 0:
+                rows[i] = True
+                cols[j] = True
+
+    # row
+    for i in range(n):
+        if rows[i]:
+            matrix[i] = [0] * m
+    # col
+    for j in range(m):
+        if cols[j]:
+            for i in range(n):
+                matrix[i][j] = 0
+
+
+# store into matrix first row and col ,TC:O(M*N), SC:O(1)
+def setZeroes4(matrix):
+    """
+    Do not return anything, modify matrix in-place instead.
+    """
+    # store zero position in rows and cols
+    n = len(matrix)
+    m = len(matrix[0])
+    extra = False # store for first col
+    # rows = [False] * n
+    # cols = [False] * m
+    for i in range(n):
+        for j in range(m):
+            if matrix[i][j] == 0:
+                matrix[i][0] = 0
+                #matrix[0][j] = 0
+                if j==0:
+                    extra = True
+                elif j > 0:
+                    matrix[0][j] = 0
+    # col
+    for j in range(1,m):
+        if matrix[0][j] == 0:
+            for i in range(n):
+                matrix[i][j] = 0
+    # row
+    for i in range(n):
+        if matrix[i][0] == 0:
+            matrix[i] = [0] * m
+    # extra
+    if extra:
+        for i in range(n):
+            matrix[i][0] = 0
