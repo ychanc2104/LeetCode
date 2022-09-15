7e1cba8a9d6247444e2cbae49073be5b1f8b2241
pine <ychanc2104@gmail.com>
2022-09-15 18:52:23 +0800

    add Manacher algorithm solution

--- Longest Palindromic Substring.py
+++ Longest Palindromic Substring.py
@@ -2,64 +2,44 @@
 # https://cppext.com/?p=1743
 
 
-class Solution:
-    def __init__(self, s):
-        self.ans_1 = self.longestPalindrome_1(s)
-        self.ans_2 = self.longestPalindrome_2(s)
 
-    def longestPalindrome_1(self, s):
-        n = len(s)
-        for i in range(n):
-            for j in range(i + 1):
-                s_test = s[j:n - i + j]
-                if s_test == s_test[::-1]:
-                    return s_test
-    def longestPalindrome_2(self, s): ## Manacher algorithm
-        ## https://havincy.github.io/blog/post/ManacherAlgorithm/
-        ## calculate palindromic radius
-        T = '#'.join('^{}$'.format(s))
-        n = len(T)
-        P = [0] * n
-        Imax = 0
-        radius = 0
-        for i in range(n-1):
-            j = 0
-            while T[i-(j+1)] == T[i+(j+1)]:
-                P[i] += 1
-                j += 1
-            if i != 0 and P[i] > radius:
-                Imax = i
-                radius = P[i]
-        s_sub = s[(Imax-radius)//2:(Imax+radius)//2]
-        return s_sub
 
-    # https: // cppext.com /?p = 1743
-    def longestPalindrome_3(self, s):  ## Manacher algorithm
-    # Transform S into T.
-        # For example, S = "abba", T = "^#a#b#b#a#$".
-        # ^ and $ signs are sentinels appended to each end to avoid bounds checking
-        T = '#'.join('^{}$'.format(s))
-        n = len(T)
-        P = [0] * n
-        C = R = 0
-        for i in range(1, n - 1):
-            P[i] = (R > i) and min(R - i, P[C-(i-C)])  # equals to i' = C - (i-C), mirror pos
-            # Attempt to expand palindrome centered at i
-            while T[i + 1 + P[i]] == T[i - 1 - P[i]]:
-                P[i] += 1
+def longestPalindrome1(s):
+    n = len(s)
+    for i in range(n):
+        for j in range(i + 1):
+            s_test = s[j:n - i + j]
+            if s_test == s_test[::-1]:
+                return s_test
 
-            # If palindrome centered at i expand past R,
-            # adjust center based on expanded palindrome.
-            if i + P[i] > R:
-                C, R = i, i + P[i]
 
-        # Find the maximum element in P.
-        maxLen, centerIndex = max((n, i) for i, n in enumerate(P))
-        return s[(centerIndex - maxLen) // 2: (centerIndex + maxLen) // 2]
+# https: // cppext.com /?p = 1743
+def longestPalindrome3(s):  ## Manacher algorithm
+# Transform S into T.
+    # For example, S = "abba", T = "^#a#b#b#a#$".
+    # ^ and $ signs are sentinels appended to each end to avoid bounds checking
+    T = '#'.join('^{}$'.format(s))
+    n = len(T)
+    P = [0] * n
+    C = R = 0
+    for i in range(1, n - 1):
+        P[i] = (R > i) and min(R - i, P[C-(i-C)])  # equals to i' = C - (i-C), mirror pos
+        # Attempt to expand palindrome centered at i
+        while T[i + 1 + P[i]] == T[i - 1 - P[i]]:
+            P[i] += 1
+
+        # If palindrome centered at i expand past R,
+        # adjust center based on expanded palindrome.
+        if i + P[i] > R:
+            C, R = i, i + P[i]
+
+    # Find the maximum element in P.
+    maxLen, centerIndex = max((n, i) for i, n in enumerate(P))
+    return s[(centerIndex - maxLen) // 2: (centerIndex + maxLen) // 2]
 
 
 # expand around center, TC:O(n^2), SC:(1)
-def longestPalindrome(s: str) -> str:
+def longestPalindrome4(s: str) -> str:
     resL = 0
     center = 0
     # len = n + n-1 = 2n-1
@@ -80,7 +60,7 @@ def longestPalindrome(s: str) -> str:
     return s[center-resL+1:center+resL].replace('#', '')
 
 # Manacher algorithm, TC:O(n), SC:(n)
-def longestPalindrome2(s: str) -> str:
+def longestPalindrome5(s: str) -> str:
 # Transform S into T.
     # For example, S = "abba", T = "^#a#b#b#a#$".
     # ^ and $ signs are sentinels appended to each end to avoid bounds checking
@@ -108,5 +88,30 @@ def longestPalindrome2(s: str) -> str:
     return T[centerIndex - maxLen+1:centerIndex + maxLen].replace('#', '')
 
 
+# Manacher algorithm, TC:O(n), SC:(n)
+def longestPalindrome6(s: str) -> str:
+    T = '#'.join('^{}$'.format(s))
+    n = len(T)
+    P = [0] * n  # dp to store radius
+    center = right_bound = 0
+    maxI = maxRadius = 0
+    for i in range(1,n - 1):
+        if right_bound > i:  # not exceed
+            # use mirror or right_bound - current_center(i)
+            P[i] = min(P[center - (i - center)], right_bound - i)
+        # start from P{i}+1 and P[i]-1
+        while T[i - P[i] - 1] == T[i + P[i] + 1]:
+            P[i] += 1
+        if i + P[i] > right_bound:
+            # update new center and right_bound
+            center = i
+            right_bound = P[i] + i
+            # record max radius and its index
+            if P[i] > maxRadius:
+                maxI, maxRadius = i, P[i]
+    return s[(maxI - maxRadius) // 2: (maxI + maxRadius) // 2]
+
+
 s = "aababa"
-A = Solution(s)
+
+
