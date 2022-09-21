6c22e908f9f0d9a99ba739d91487f23a4b81ec9c
pine <ychanc2104@gmail.com>
2022-09-21 14:28:04 +0800

    add iterative solution

--- Reverse Nodes in k-Group.py
+++ Reverse Nodes in k-Group.py
@@ -31,3 +31,43 @@ def reverseKGroup(head: Optional[ListNode], k: int) -> Optional[ListNode]:
             head.next = reverseKGroup(dummy, k)  # connect to next reverse linked lists
             return reverse
     return head
+
+
+
+# iterative method, TC:O(N), SC:O(1)
+def reverseKGroup2(head: Optional[ListNode], k: int) -> Optional[ListNode]:
+    def reversek(head, k):
+        dummy = head
+        reverse = None
+        while k != 0:
+            dummy_next = dummy.next
+            dummy.next = reverse
+            reverse = dummy
+            dummy = dummy_next
+            k -= 1
+        return reverse
+
+    dummy = head
+    prev = None
+    ktail = None
+    while dummy:
+        count = 0
+        dummy = head
+        while k > count and dummy:
+            dummy = dummy.next
+            count += 1
+        # reverse k nodes
+        if k == count:
+            reverse = reversek(head, k)
+            # store result
+            if not prev:
+                prev = reverse
+            # connect original first node to second reverse node
+            if ktail:
+                ktail.next = reverse
+            ktail = head
+            head = dummy
+    # still remain some nodes no need to reverse
+    if ktail:
+        ktail.next = head
+    return prev if prev else head
