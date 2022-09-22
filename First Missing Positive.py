cc8af3104df9c5069cdea68508f4f5625cf77c58
pine <ychanc2104@gmail.com>
2022-09-22 14:58:37 +0800

    First Missing Positive

---
+++ First Missing Positive.py
@@ -0,0 +1,44 @@
+# https://leetcode.com/problems/first-missing-positive/
+# https://leetcode.com/problems/first-missing-positive/discuss/872448/Python-O(n)-solution-with-constant-space-EXPLAINED-with-clear-train-of-thoughts
+# https://leetcode.com/problems/first-missing-positive/discuss/17080/Python-O(1)-space-O(n)-time-solution-with-explanation
+
+
+# use num as index, TC:O(n), SC:O(n)
+def firstMissingPositive(nums) -> int:
+    # answer must be between 1 and n+1
+    n = len(nums)
+    # clean data
+    for i in range(n):
+        if nums[i] > n:
+            nums[i] = 0
+
+    flags = [True] * n
+    for num in nums:
+        if 0 < num:  # ignore negative
+            flags[(num - 1) % n] = False
+
+    for i in range(n):
+        if flags[i]:
+            return i + 1
+    return n + 1
+
+
+# TC:O(n), SC:O(1)
+def firstMissingPositive2(nums) -> int:
+    # answer must be between 1 and n+1
+    n = len(nums)
+    # clean data, make all data is between 0 and n
+    for i in range(n):
+        if nums[i] > n or nums[i] < 0:
+            nums[i] = 0
+    # index i means does i+1 appear
+    for num in nums:
+        index = (num) % (n+1) - 1
+        if num > 0 and index >= 0: # ignore negative and zero, and index < 0
+            # prevent affect the index not used
+            nums[(num) % (n+1) - 1] += n+1 # add n+1 to prevent original position is 0
+    #print(nums)
+    for i in range(n):
+        if nums[i] <= n:
+            return i+1
+    return n+1
