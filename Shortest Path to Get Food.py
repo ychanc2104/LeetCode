3169630236847330e0504b004917884ed27f245a
pine <ychanc2104@gmail.com>
2022-10-17 12:00:13 +0800

    add a BFS solution

--- Shortest Path to Get Food.py
+++ Shortest Path to Get Food.py
@@ -1,6 +1,8 @@
 # https://leetcode.com/problems/shortest-path-to-get-food/
 # https://leetcode.com/problems/shortest-path-to-get-food/discuss/1027023/PythonC%2B%2B-clean-BFS-method-O(MN)
 
+
+import collections
 # TC: O(MN), SC:O(M+N)
 def getFood(grid) -> int:
     # find '*'
@@ -27,4 +29,28 @@ def getFood(grid) -> int:
                     return step
         queue = leafs
         step += 1
+    return -1
+
+# BFS, TC: O(MN), SC:O(M+N)
+def getFood2(grid) -> int:
+    # bfs
+    # find person
+    queue = collections.deque([])
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
+            if grid[i][j] == '*':
+                queue.append(((i, j), 0))
+                break
+
+    while queue:
+        (r, c), step = queue.popleft()
+        # explore four directions
+        if r < 0 or c < 0 or r >= len(grid) or c >= len(grid[0]) or grid[r][c] == 'X':
+            continue
+        if grid[r][c] == '#':
+            return step
+        grid[r][c] = 'X'
+        for ro, co in ((1, 0), (-1, 0), (0, 1), (0, -1)):
+            rn, cn = r + ro, c + co
+            queue.append(((rn, cn), step + 1))
     return -1
