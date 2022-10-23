e001a9d4d7a0dc19b5a60483708426d7ce085298
YCC <ychanc2104@gmail.com>
2022-10-23 18:14:39 +0800

    Add math solution

--- Set Mismatch.py
+++ Set Mismatch.py
@@ -1,5 +1,7 @@
 # https://leetcode.com/problems/set-mismatch/description/
 # https://leetcode.com/problems/set-mismatch/solutions/1089732/python-o-1-space-o-n-time-tricky-super-simple/?orderBy=most_votes&languageTags=python
+# https://leetcode.com/problems/set-mismatch/solutions/1089475/python-o-n-time-o-1-space-math-solution-explained/?orderBy=most_votes
+
 
 
 # first thought, TC:O(N), SC:O(N)
@@ -30,3 +32,10 @@ def findErrorNums2(nums: List[int]) -> List[int]:
             nums[(abs(num) - 1)] *= -1
     missing = product ^ dup
     return [dup, missing]
+
+# generator expressions, TC:O(N), SC:O(1)
+def findErrorNums3(nums: List[int]) -> List[int]:
+    n = len(nums)
+    A = -sum(nums) + n * (n + 1) // 2
+    B = -sum(i * i for i in nums) + n * (n + 1) * (2 * n + 1) // 6 # generator, SC cost O(1)
+    return [(B - A * A) // 2 // A, (B + A * A) // 2 // A]
