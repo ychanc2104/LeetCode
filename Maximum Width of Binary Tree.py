728058fd817499041baf7d6e5178689bad452462
pine <ychanc2104@gmail.com>
2022-10-24 17:07:17 +0800

    add dfs and bfs solutions

--- Maximum Width of Binary Tree.py
+++ Maximum Width of Binary Tree.py
@@ -1,6 +1,8 @@
 # https://leetcode.com/problems/maximum-width-of-binary-tree/
 
 
+import collections
+
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
@@ -23,3 +25,57 @@ def widthOfBinaryTree(root) -> int:
             count = max(count, leafs[-1][1] - leafs[0][1] + 1)
         queue = leafs
     return count
+
+# level-order BFS, TC:O(N), SC:O(W)
+def widthOfBinaryTree2(root) -> int:
+    # root:0, 2i+1 and 2i+2
+
+    queue = collections.deque([(root, 0, 0)])
+    index_arr = []
+    while queue:
+        node, level, index = queue.popleft()
+        if len(index_arr) <= level:
+            index_arr.append([index])
+        else:
+            index_arr[level].append(index)
+        if node.left:
+            queue.append((node.left, level + 1, index * 2 + 1))
+        if node.right:
+            queue.append((node.right, level + 1, index * 2 + 2))
+    # print(res)
+    # width_arr = [max(arr) - min(arr) + 1 for arr in index_arr]
+    return max(max(arr) - min(arr) + 1 for arr in index_arr)
+
+# level-order BFS, TC:O(N), SC:O(W)
+def widthOfBinaryTree3(root) -> int:
+    # root:0, 2i+1 and 2i+2
+    queue = [(root, 0)]
+    res = 1
+    while queue:
+        leaves = []
+        for i in range(len(queue)):
+            node, index = queue[i]
+            if node.left:
+                leaves.append((node.left, 2*index+1))
+            if node.right:
+                leaves.append((node.right, 2*index+2))
+        if not leaves: break
+        res = max(res, leaves[-1][1] - leaves[0][1] + 1)
+        queue = leaves
+    return res
+
+
+# dfs, TC:O(N), SC:O(W)
+def widthOfBinaryTree4(root: Optional[TreeNode]) -> int:
+    # root:0, 2i+1 and 2i+2
+    index_table = collections.defaultdict(list)
+
+    def dfs(node, level=0, i=0):
+        if not node: return
+        dfs(node.left, level + 1, 2 * i + 1)
+        dfs(node.right, level + 1, 2 * i + 2)
+        index_table[level].append(i)
+
+    dfs(root)
+    # print(index_table)
+    return max(max(w) - min(w) + 1 for w in index_table.values())
