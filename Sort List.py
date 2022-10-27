5f4a2659d16c1b2149c16e97ccf9f26f9a96588a
pine <ychanc2104@gmail.com>
2022-10-27 14:57:52 +0800

    add recursive merge sort and radix sort

--- Sort List.py
+++ Sort List.py
@@ -1,5 +1,7 @@
 # https://leetcode.com/problems/sort-list/
 # https://leetcode.com/problems/sort-list/discuss/892759/Python-O(n-log-n-log-n)-merge-sort-explained
+# https://leetcode.com/problems/sort-list/solutions/1797262/radix-sort-with-on-log-n-runtime-and-o1-space/
+
 
 # Definition for singly-linked list.
 class ListNode:
@@ -100,3 +102,71 @@ def sortList2(head: Optional[ListNode]) -> Optional[ListNode]:
         size *= 2
 
     return newhead.next
+
+# recursive merged sort, TC:O(nlogn), SC:O(logn) for recursive calls
+def sortList3(head: Optional[ListNode]) -> Optional[ListNode]:
+    def findmid(head):
+        slow = head
+        fast = head.next.next
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        # 1,2,3,4,5
+        slow_next = slow.next
+        slow.next = None
+        return slow_next
+    if not head or not head.next:
+        return head
+    mid = findmid(head)
+    L = sortList3(head) # sorted
+    R = sortList3(mid) # sorted
+    # merge
+    dummy = merged = ListNode(-1)
+    while L and R:
+        if L.val <= R.val:
+            dummy.next = L
+            L = L.next
+        else:
+            dummy.next = R
+            R = R.next
+        dummy = dummy.next
+    dummy.next = L if L else R
+    return merged.next
+
+# radix sort, TC:O(nlogn), SC:O(1)
+def sortList3(head: Optional[ListNode]) -> Optional[ListNode]:
+    def reverse(node):
+        cur = None
+        nex = node
+        tail = node
+        while nex:
+            tmp = nex.next
+            nex.next = cur
+            cur = nex
+            nex = tmp
+        return cur, tail
+
+    def radix(head, exp):
+        tails = [ListNode(0) for _ in range(20)]
+        div = 10 ** exp
+        cur = head
+        while cur:
+            abs_index = (abs(cur.val) // div) % 10
+            index = 10 + abs_index * (1 if cur.val >= 0 else -1)
+            tmp = cur.next
+            cur.next = tails[index].next
+            tails[index].next = cur
+            cur = tmp
+        dummy = ListNode(0)
+        cur = dummy
+
+        for tail in tails:
+            if tail.next:
+                h, t = reverse(tail.next)
+                cur.next = h
+                cur = t
+        return dummy.next
+
+    for i in range(0, 5):
+        head = radix(head, i)
+    return head
