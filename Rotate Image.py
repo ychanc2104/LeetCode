51f296843c0da9c0e1ffbd9f5d2f178358fb297d
YCC <ychanc2104@gmail.com>
2022-11-03 16:27:21 +0800

    rotate by groups

--- Rotate Image.py
+++ Rotate Image.py
@@ -72,6 +72,24 @@ def rotate2(matrix):
     matrix = zip(*matrix[::-1])
     return matrix
 
+# rotate by groups, TC:O(MN), SC:O(1)
+def rotate3(matrix):
+    """
+    Do not return anything, modify matrix in-place instead.
+    """
+    n = len(matrix)
+    for i in range(n//2 + n%2):
+        for j in range(n//2):
+            temp = matrix[n-j-1][i]
+            # right-bottom to left-bottom
+            matrix[n-j-1][i] = matrix[n-i-1][n-j-1]
+            # right-top to right-bottom
+            matrix[n-i-1][n-j-1] = matrix[j][n-i-1]
+            # left-top to right-top
+            matrix[j][n-i-1] = matrix[i][j]
+            # final
+            matrix[i][j] = temp
+
 
 matrix = [[1,2,3],[4,5,6],[7,8,9]]
 
