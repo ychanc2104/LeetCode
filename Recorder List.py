57c94d961e69ea78e2ef96fdb4338747f819423e
YCC <ychanc2104@gmail.com>
2022-11-07 17:39:01 +0800

    add recursive solution

--- Recorder List.py
+++ Recorder List.py
@@ -2,6 +2,8 @@
 # https://leetcode.com/problems/reorder-list/discuss/801883/Python-3-steps-to-success-explained
 # https://leetcode.com/problems/reorder-list/discuss/801971/Python-O(n)-by-two-pointers-w-Visualization
 # https://leetcode.com/problems/reorder-list/discuss/567955/Python-2-solutions%3A-Stack-3-steps-Clean-and-Concise
+# https://leetcode.com/problems/reorder-list/solutions/1640556/c-easy-to-solve-beginner-friendly-with-detailed-explanation-and-dry-run/
+
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
@@ -68,3 +70,52 @@ def reorderList2(head):
         temp_head = temp_head.next # 2
         # assign head.next.next to previous head.next
         temp_prev = head_next
+
+# recurisve,
+def reorderList3(head: Optional[ListNode]) -> None:
+    """
+    Do not return anything, modify head in-place instead.
+    """
+    def helper(head, dummy):
+        if not head.next: return dummy
+        temp = helper(head.next, dummy)
+        if not temp or not temp.next or temp == head:
+            return None
+        temp_next = temp.next
+        temp.next = head.next
+        head.next = None
+        temp.next.next = temp_next
+        return temp_next
+    dummy = head
+    helper(head, dummy)
+
+
+# TC: O(N), SC:O(1)
+def reorderList4(head):
+    """
+    Do not return anything, modify head in-place instead.
+    """
+    if not head or not head.next:
+        return
+    # find mid => reverse 2nd part => merge two
+    slow = head
+    fast = head.next.next
+    while fast and fast.next:
+        slow = slow.next
+        fast = fast.next.next
+    mid = slow.next
+    slow.next = None
+    # reverse mid
+    reverse = None
+    while mid:
+        temp = mid.next
+        mid.next = reverse
+        reverse = mid
+        mid = temp
+    # merge, reverse and head, size reverse >= head
+    prev = head
+    while prev and reverse:
+        temp = prev.next
+        prev.next = reverse
+        prev = prev.next
+        reverse = temp # switching between two linked list
