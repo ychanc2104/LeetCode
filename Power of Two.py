04e389d016eec80e908f1355a78b2a9481b8e02c
YCC <ychanc2104@gmail.com>
2022-11-14 18:43:28 +0800

    add two solutions

--- Power of Two.py
+++ Power of Two.py
@@ -3,3 +3,16 @@
 # use rightmost bit, TC:O(1), SC:O(1)
 def isPowerOfTwo(n: int) -> bool:
     return n == (n & -n) and n
+
+# logN
+def isPowerOfTwo2(n: int) -> bool:
+    if n == 0: return False
+    elif n == 1: return True
+    elif n % 2: return False
+    return isPowerOfTwo2(n/2)
+
+# logN
+def isPowerOfTwo3(n: int) -> bool:
+    if n == 1: return True
+    elif n < 1: return False
+    return isPowerOfTwo3(n/2)
