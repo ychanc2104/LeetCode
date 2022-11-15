55dabc82878f4367799e84c3010f1c948a800b73
YCC <ychanc2104@gmail.com>
2022-11-15 13:30:10 +0800

    add a solution

--- Lowest Common Ancestor of a Binary Tree.py
+++ Lowest Common Ancestor of a Binary Tree.py
@@ -3,16 +3,23 @@
 # recursive soln https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/discuss/65245/Iterative-Solutions-in-PythonC%2B%2B
 
 ## TC: O(N)
-class Solution:
-    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        # return the node if find the target
-        # return node if encounter else return None
-        # return current node if left and right are not None
-        if not root: return root
-        if root == p or root == q:
-            return root
-        left = self.lowestCommonAncestor(root.left, p, q)
-        right = self.lowestCommonAncestor(root.right, p, q)
-        if left and right:
-            return root
-        return left if left else right
+def lowestCommonAncestor(root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+    # return the node if find the target
+    # return node if encounter else return None
+    # return current node if left and right are not None
+    if not root: return root
+    if root == p or root == q:
+        return root
+    left = lowestCommonAncestor(root.left, p, q)
+    right = lowestCommonAncestor(root.right, p, q)
+    if left and right:
+        return root
+    return left if left else right
+
+# concise
+def lowestCommonAncestor2(root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+    if not root or root == p or root == q:
+        return root
+    L = lowestCommonAncestor2(root.left, p, q)
+    R = lowestCommonAncestor2(root.right, p ,q)
+    return root if L and R else L or R
