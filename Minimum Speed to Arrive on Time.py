b713d0f61fbfb65c5c46951630c5d511b2562e9e
YCC <ychanc2104@gmail.com>
2022-11-30 00:50:18 +0800

    fix float rounding error

--- Minimum Speed to Arrive on Time.py
+++ Minimum Speed to Arrive on Time.py
@@ -1,11 +1,11 @@
 # https://leetcode.com/problems/minimum-speed-to-arrive-on-time/
 
-# binary serach, TC:O(), SC:O()
+# binary serach, TC:O(Nlogk) k is max speed, SC:O(1)
 def minSpeedOnTime(dist: list[int], hour: float) -> int:
     def computeTime(speed):
-        t = dist[-1] / speed
-        t += sum(int((dist[i] - 1) / speed) + 1 for i in range(len(dist) - 1))
-        return t
+        t = sum((dist[i] - 1) // speed + 1 for i in range(len(dist) - 1))
+        # add float in the end to prevent float rounding error
+        return t + dist[-1] / speed
 
     # (L,R,mid,t) 1,3,2,4 (move R)=> 1,1,1,6 (move R) => 0,1 stop
     max_speed = 10 ** 7  # upper bound
