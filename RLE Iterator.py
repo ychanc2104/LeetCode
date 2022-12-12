96a80db6085d6bd3c2b99a998f876a653ef2ea3c
YCC <ychanc2104@gmail.com>
2022-12-13 03:04:40 +0800

    add a solution

--- RLE Iterator.py
+++ RLE Iterator.py
@@ -26,3 +26,33 @@ class RLEIterator:
         self.n += n
         idx = bsearch(self.cum, self.n)
         return self.nums[idx] if idx < len(self.cum) else -1
+
+# record last L
+class RLEIterator:
+
+    def __init__(self, encoding: List[int]):
+        self.cum = []  # 3,3,5
+        self.nums = []  # 8,9,5
+        self.n = 0
+        self.prev_L = 0
+        s = 0
+        for i in range(0, len(encoding), 2):  # 0,6,2  => 0,2,4
+            s += encoding[i]
+            self.cum.append(s)
+            self.nums.append(encoding[i + 1])
+
+    def next(self, n: int) -> int:
+        def bsearch(cum, target, L):
+            R = len(cum) - 1
+            while L <= R:
+                mid = (L + R) // 2
+                if cum[mid] >= target:
+                    R = mid - 1
+                else:
+                    L = mid + 1
+            return L  # [0, len(cum)]
+
+        idx = bsearch(self.cum, self.n + n, self.prev_L)
+        self.n += n
+        self.prev_L = idx
+        return self.nums[idx] if idx < len(self.cum) else -1
