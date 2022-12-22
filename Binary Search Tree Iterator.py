e851dc16c195d538e4087ea40d6a0d439fbce05e
YCC <ychanc2104@gmail.com>
2022-12-22 21:18:26 +0800

    add a generator solution

--- Binary Search Tree Iterator.py
+++ Binary Search Tree Iterator.py
@@ -6,8 +6,11 @@ class TreeNode:
         self.val = val
         self.left = left
         self.right = right
+
+# use stack
 class BSTIterator:
 
+    # TC:O(logN) for construction, SC:O(logN)
     def __init__(self, root: Optional[TreeNode]):
         # in-order, left=>root=>right
         self.stack = [] # max to store O(h) = O(logN)
@@ -26,6 +29,38 @@ class BSTIterator:
         if root:
             self.stack.append(root)
             self.dfs(root.left)
+
+# use generator
+class BSTIterator2:
+
+    # TC:O(N) for construction, SC:O(1)
+    def __init__(self, root: Optional[TreeNode]):
+        # in-order, left=>root=>right
+        self.stack = [] # size one
+        def dfs(root):
+            if root:
+                yield from dfs(root.left)
+                yield root
+                yield from dfs(root.right)
+        self.iterator = dfs(root)
+
+    # TC:O(1), SC:O(1)
+    def next(self) -> int:
+        if self.stack:
+            val = self.stack.pop().val
+        else:
+            val = next(self.iterator, TreeNode()).val
+        return val
+
+    # TC:O(1), SC:O(1)
+    def hasNext(self) -> bool:
+        if self.stack:
+            return True
+        node = next(self.iterator, None)
+        if not node: return False
+        self.stack.append(node)
+        return True
+
 
 # Your BSTIterator object will be instantiated and called as such:
 # obj = BSTIterator(root)
