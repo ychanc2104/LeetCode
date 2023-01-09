c5ea8343109279ef24e1257a3929e52c2a7d2d49
YCC <ychanc2104@gmail.com>
2023-01-10 01:39:57 +0800

    add two solutions

--- Serialize and Deserialize BST.py
+++ Serialize and Deserialize BST.py
@@ -1,6 +1,9 @@
 # https://leetcode.com/problems/serialize-and-deserialize-bst/
 # https://leetcode.com/problems/serialize-and-deserialize-bst/discuss/93224/Concise-iterative-Python-solution-using-stack-beat-99.
+# https://leetcode.com/problems/serialize-and-deserialize-bst/solutions/93171/python-o-n-solution-easy-to-understand/
 
+
+import collections
 
 # Definition for a binary tree node.
 # class TreeNode:
@@ -9,7 +12,78 @@
 #         self.left = None
 #         self.right = None
 
+# use preorder
 class Codec:
+
+    def serialize(self, root: Optional[TreeNode]) -> str:
+        """Encodes a tree to a single string.
+        """
+        # root => left => right, preorder
+        res = []
+        def dfs(root):
+            if not root:
+                res.append('#')
+                return
+            res.append(str(root.val))
+            dfs(root.left)
+            dfs(root.right)
+        dfs(root)
+        return ','.join(res)
+
+    def deserialize(self, data: str) -> Optional[TreeNode]:
+        """Decodes your encoded data to tree.
+        """
+        # 0,1,2,3,4,5,6
+        data_list = collections.deque(data.split(','))
+        def helper():
+            if not data_list:
+                return None
+            if data_list[0] == '#':
+                data_list.popleft()
+                return None
+            root = TreeNode(int(data_list.popleft()))
+            root.left = helper()
+            root.right = helper()
+            return root
+        return helper()
+
+# use BST property
+class Codec2:
+
+    def serialize(self, root: Optional[TreeNode]) -> str:
+        """Encodes a tree to a single string.
+        """
+        # root => left => right, preorder
+        res = []
+
+        def dfs(root):
+            if root:
+                res.append(str(root.val))
+                dfs(root.left)
+                dfs(root.right)
+
+        dfs(root)
+        return ','.join(res)
+
+    # use BST property
+    def deserialize(self, data: str) -> Optional[TreeNode]:
+        """Decodes your encoded data to tree.
+        """
+        if not data: return None
+        data_list = collections.deque(int(x) for x in data.split(','))
+
+        def helper(MIN=float('-inf'), MAX=float('inf')):
+            if not data_list or not MIN < data_list[0] < MAX:
+                return None
+            val = data_list.popleft()
+            root = TreeNode(val)
+            root.left = helper(MIN, val)
+            root.right = helper(val, MAX)
+            return root
+
+        return helper()
+
+class Codec3:
 
     def serialize(self, root: Optional[TreeNode]) -> str:
         """Encodes a tree to a single string.
@@ -51,7 +125,7 @@ class Codec:
 
 
 ## use construct binary tree from preorder and inorder traversal
-class Codec2:
+class Codec4:
 
     def serialize(self, root: Optional[TreeNode]) -> str:
         """Encodes a tree to a single string.
