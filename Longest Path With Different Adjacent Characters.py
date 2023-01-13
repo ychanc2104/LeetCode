92f74ba2184ff8203b2cf21016030fa58c787ac2
YCC <ychanc2104@gmail.com>
2023-01-13 23:36:28 +0800

    Longest Path With Different Adjacent Characters

---
+++ Longest Path With Different Adjacent Characters.py
@@ -0,0 +1,56 @@
+# https://leetcode.com/problems/longest-path-with-different-adjacent-characters/description/
+# https://leetcode.com/problems/longest-path-with-different-adjacent-characters/solutions/1955433/java-c-python-dfs-on-tree/
+# https://leetcode.com/problems/longest-path-with-different-adjacent-characters/solutions/3042992/python3-dfs-explained/
+
+import collections
+
+
+# dfs, TC:O(NlogN) with sort, SC:O(N)
+
+def longestPath(parent: List[int], s: str) -> int:
+    res = [1]
+    graph = collections.defaultdict(list)
+    for node, p in enumerate(parent):
+        if p == -1: continue
+        graph[node].append(p)
+        graph[p].append(node)
+
+    def dfs(node=0, parent=-1):  # return max path
+        candidates = []
+        for child in graph[node]:
+            if child == parent: continue  # don't go back to parent
+            temp = dfs(child, node)
+            if s[node] != s[child]:  # no adjacent node is the same char
+                candidates.append(temp)
+        candidates.sort()
+        res[0] = max(res[0], sum(candidates[-2:]) + 1)
+        return (candidates[-1] if candidates else 0) + 1
+
+    dfs()
+    return res[0]
+
+# dfs, TC:O(N) w/o sort, SC:O(N)
+def longestPath2(parent: List[int], s: str) -> int:
+    res = 1
+    graph = collections.defaultdict(list)
+    for node, p in enumerate(parent):
+        if p == -1: continue
+        graph[node].append(p)
+        graph[p].append(node)
+
+    def dfs(node=0, parent=-1):  # return max path
+        nonlocal res
+        max1 = max2 = 0
+        for child in graph[node]:
+            if child == parent: continue  # don't go back to parent
+            temp = dfs(child, node)
+            if s[node] != s[child]:  # no adjacent node is the same char
+                if temp >= max1:
+                    max1, max2 = temp, max1
+                elif temp > max2:
+                    max2 = temp
+        res = max(res, max1 + max2 + 1)
+        return max1 + 1
+
+    dfs()
+    return res
