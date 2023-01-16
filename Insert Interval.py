4f2e837a0f3cf555cc4dced198c2a5aa1a4076e9
YCC <ychanc2104@gmail.com>
2023-01-16 16:53:09 +0800

    update solutions

--- Insert Interval.py
+++ Insert Interval.py
@@ -3,100 +3,8 @@
 
 # separate into (left + newInterval + right)
 
-## T: O(nlogn)
+## T:O(n), SC:O(N)
 def insert(intervals, newInterval):
-    intervals.append(newInterval)
-    intervals = sorted(intervals, key=lambda x :x[0])
-    res = [intervals[0]]
-    for num in intervals:
-        if res[-1][1 ]>=num[0]:
-            res[-1][1] = max(res[-1][1], num[1])
-        else:
-            res.append(num)
-    return res
-
-## T:O(n)
-def insert2(intervals, newInterval):
-    I = [newInterval]
-    res = []
-    for num in intervals:
-        if I:
-            ## overlapping with I
-            if not ((num[1] < I[0][0]) or (num[0] > I[0][1])):
-                # print('overlapping')
-                # update I
-                I[0] = [min(I[0][0], num[0]), max(I[0][1], num[1])]
-            ## not overlapping
-            else:
-                # check small or bigger than num[0]
-                if I[0][0] < num[0]:
-                    res.append(I[0])
-                    I.pop()
-                    res.append(num)
-                else:
-                    res.append(num)
-        else:  #
-            res.append(num)
-    if I:
-        res.append(I[0])
-    return res
-
-
-## T:O(n)
-def insert3(intervals, newInterval):
-    left = []
-    k = 0
-
-    for num in intervals:
-        ## overlapping, merge
-        if not (num[0] > newInterval[1] or num[1] < newInterval[0]):
-            newInterval = [min(newInterval[0], num[0]), max(newInterval[1], num[1])]
-            k += 1
-        ## non-overlapping
-        else:
-            # left case
-            if num[0] < newInterval[0]:
-                left.append(num)
-                k += 1
-    return left + [newInterval] + intervals[k:]
-
-## T:O(n)
-def insert4(intervals, I):
-    res, i = [], -1
-    for i, (x, y) in enumerate(intervals):
-        if y < I[0]:
-            res.append([x, y])
-        elif I[1] < x:
-            i -= 1
-            break
-        else:
-            I[0] = min(I[0], x)
-            I[1] = max(I[1], y)
-    return res + [I] + intervals[i + 1:]
-
-## T:O(n), SC:O(1)
-def insert5(intervals, newInterval):
-    res = []
-    L, R = newInterval
-    isInsert = False
-    for s, e in intervals:
-        if s <= L <= e or s <= R <= e or (L <= s and R >= e):
-            # overlapping
-            L, R = min(s, L), max(e, R)
-        else:
-            # not overlapping
-            if not isInsert and s > L:
-                res.append([L, R])
-                isInsert = True
-            res.append([s, e])
-    # edge case for len(intervals)<=1
-    if not isInsert:
-        res.append([L, R])
-    return res
-
-
-## T:O(n), SC:O(1)
-def insert6(intervals, newInterval):
     ## separate into three parts, left + merged newInterval + rest of intervals, put overlapping case to the 'else'
     res = []
     i = 0
@@ -116,7 +24,7 @@ def insert6(intervals, newInterval):
 
 
 ## T:O(n), SC:O(1)
-def insert7(intervals, newInterval):
+def insert2(intervals, newInterval):
     ## separate into three parts, left + merged newInterval + rest of intervals, put overlapping case to the 'else'
     k = 0
     i = 0
@@ -131,6 +39,20 @@ def insert7(intervals, newInterval):
             newInterval = [min(s, newInterval[0]), max(e, newInterval[1])]
         i += 1
     return intervals[:k] + [newInterval] + intervals[i:]
+
+def insert3(intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+    j = 0
+    k = len(intervals)
+    for i,(s,e) in enumerate(intervals):
+        if e < newInterval[0]: # smaller than inserted
+            j += 1 # move pointer
+        elif s <= newInterval[1]: # overlapping
+            newInterval = [min(newInterval[0], s), max(newInterval[1], e)]
+        else: # non-overlapping
+            k = i
+            break
+    return intervals[:j] + [newInterval] + intervals[k:]
+
 
 intervals = [[1,2],[3,5],[6,7],[8,10],[12,16]]
 
