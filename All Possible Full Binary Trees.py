b2da8639ad46dda27e285129403a524a55fdf3bc
YCC <ychanc2104@gmail.com>
2023-01-16 16:53:29 +0800

    add a solution

--- All Possible Full Binary Trees.py
+++ All Possible Full Binary Trees.py
@@ -1,7 +1,8 @@
 # https://leetcode.com/problems/all-possible-full-binary-trees/description/
 
+import functools
 
-#
+
 # Definition for a binary tree node.
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
@@ -24,3 +25,24 @@ def allPossibleFBT(n: int) -> List[Optional[TreeNode]]:
         memo[n] = res
         return memo[n]
     return helper(n)
+
+# recursive, TC:O(2^n), SC:O(2^N)
+def allPossibleFBT2(n: int) -> List[Optional[TreeNode]]:
+    # 3:1, 5:2, 7:5
+    @functools.lru_cache(None)
+    def dfs(i): # full binary tree
+        if i == 0:
+            return [None]
+        if i == 1:
+            return [TreeNode(0)]
+        if i % 2 == 0:
+            return []
+        res = []
+        for j in range(1, i, 2): # i=1,3,5
+            for left in dfs(i-j-1):
+                for right in dfs(j):
+                    res.append(TreeNode(0, left, right))
+        return res
+    # 5=> 1,1,3 1,3,1
+    # 7=> 1,1,5(2) 1,3,3 1,5,1(2)
+    return dfs(n) 
