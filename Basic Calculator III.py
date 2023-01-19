18e8d948f7550e19483c6b84feb507b0556daf65
YCC <ychanc2104@gmail.com>
2023-01-19 17:58:55 +0800

    add a postfix solution

--- Basic Calculator III.py
+++ Basic Calculator III.py
@@ -35,3 +35,44 @@ def calculate(s: str) -> int:
                 curnum = 0
         # print(stack)
     return sum(stack)
+
+
+# use two stacks, TC:O(N), SC:O(N)
+def calculate2(s: str) -> int:
+    equations = {'+':operator.add, '-':operator.sub, '*':operator.mul, '/':operator.truediv}
+    def evaluate():
+        a, b = nums.pop(), nums.pop()
+        num = equations[opts.pop()](b, a)
+        nums.append(int(num))
+
+    curnum = 0
+    nums = [] # store confirmed value
+    opts = []
+    for e in s:
+        if e in '0123456789':
+            curnum = curnum*10 + int(e)
+        elif e == '(':
+            opts.append(e)
+        elif e in '+-': # can process all
+            nums.append(curnum)
+            curnum = 0
+            while opts and opts[-1] in '+-*/':
+                evaluate()
+            opts.append(e)
+        elif e in '*/':
+            nums.append(curnum)
+            curnum = 0
+            while opts and opts[-1] in '*/': # if opts and opts[-1] in '*/':
+                evaluate()
+            opts.append(e)
+        elif e == ')':
+            nums.append(curnum)
+            while opts[-1] != '(':
+                evaluate()
+            curnum = nums.pop()
+            opts.pop() # remove (
+    # print(curnum, nums, opts)
+    nums.append(curnum)
+    while opts:
+        evaluate()
+    return nums[0]
