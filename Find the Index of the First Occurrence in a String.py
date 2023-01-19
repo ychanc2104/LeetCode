c292c2bd42cf5a972826482543e273c2f8562b2c
YCC <ychanc2104@gmail.com>
2023-01-19 21:53:45 +0800

    add a KMP solution

--- Find the Index of the First Occurrence in a String.py
+++ Find the Index of the First Occurrence in a String.py
@@ -1,8 +1,35 @@
 # https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string/description/
 # https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string/solutions/978984/python-rabin-karp/
 
+
 # Knuth-Morris-Pratt algorithm(KMP), TC:O(M+N), SC:O(M)
 def strStr(haystack: str, needle: str) -> int:
+    n, m = len(haystack), len(needle)
+    failure_fun = [0] * m
+
+    # abaaba => [0,0,1,1,2,1]
+    for i in range(1, m):
+        idx = failure_fun[i - 1]
+        while idx > 0 and needle[i] != needle[idx]:
+            idx = failure_fun[idx - 1]  # try next match or not
+        failure_fun[i] = idx + (needle[i] == needle[idx])
+    j = 0  # haystack pointer
+    k = 0  # needle pointer
+    while j < n:
+        if haystack[j] == needle[k]:
+            j += 1
+            k += 1
+            if k == m:  # finish
+                return j - m
+        else:
+            if k == 0:
+                j += 1  # move to next str in haystack
+            else:
+                k = failure_fun[k - 1]  # move to previous
+    return -1  # not matched
+
+# Knuth-Morris-Pratt algorithm(KMP), TC:O(M+N), SC:O(M)
+def strStr2(haystack: str, needle: str) -> int:
     n, m = len(haystack), len(needle)
     failure_fun = [0] * m
 
@@ -35,7 +62,7 @@ def strStr(haystack: str, needle: str) -> int:
     return -1
 
 # use Rabin-Karp rolling hash, TC:O(N), SC:O(1)
-def strStr2(haystack: str, needle: str) -> int:
+def strStr3(haystack: str, needle: str) -> int:
     def f(c):
         return ord(c)-ord('A')
 
@@ -53,7 +80,7 @@ def strStr2(haystack: str, needle: str) -> int:
 
 
 # use Rabin-Karp rolling hash, TC:O(N), SC:O(1)
-def strStr3(haystack: str, needle: str) -> int:
+def strStr4(haystack: str, needle: str) -> int:
     # rolling hash, TC:O(N)
     to_int = lambda x: ord(x) - ord('a')
     n, m = len(haystack), len(needle)
