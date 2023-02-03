3377562212068cf43fd2d83702b5c94750130add
YCC <ychanc2104@gmail.com>
2023-02-03 20:28:21 +0800

    add a solution

--- Maximum Frequency Stack.py
+++ Maximum Frequency Stack.py
@@ -29,6 +29,28 @@ class FreqStack:
             self.maxfreq -= 1
         return val
 
+
+class FreqStack2:
+
+    def __init__(self):
+        self.freq_stack = collections.defaultdict(list)  # freq: val stack
+        self.max_f = 0
+        self.counter = collections.Counter()
+
+    def push(self, val: int) -> None:
+        self.counter[val] += 1
+        f = self.counter[val]
+        self.max_f = max(self.max_f, f)
+        self.freq_stack[f].append(val)
+
+    def pop(self) -> int:
+        val = self.freq_stack[self.max_f].pop()
+        self.counter[val] -= 1
+        if not self.freq_stack[self.max_f]:
+            self.max_f = self.counter[val]
+        return val
+
+
 # Your FreqStack object will be instantiated and called as such:
 # obj = FreqStack()
 # obj.push(val)
