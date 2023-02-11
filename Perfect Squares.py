475b5e19aab063d4f053f50491eeb6a4cf77306b
YCC <ychanc2104@gmail.com>
2023-02-12 01:49:56 +0800

    add a solution

--- Perfect Squares.py
+++ Perfect Squares.py
@@ -1,8 +1,8 @@
 # https://leetcode.com/problems/perfect-squares/description/
 
-import collections
+import collections, functools
 
-# first thought, dp, TC:O(n*sqrt(n)), SC:O(N)
+# bottom-up dp, TC:O(n*sqrt(n)), SC:O(N)
 def numSquares(n: int) -> int:
     # 6 => dp[1]+dp[5], dp[2]+dp[4], 2*dp[3]
     dp = [float("inf")] * (n + 1)  # first perfect
@@ -15,8 +15,24 @@ def numSquares(n: int) -> int:
                 dp[i] = min(dp[i], dp[square] + dp[i - square])
     return dp[-1]
 
-# greedy + bfs, TC:O(n^(h/2)) tree_node * tree_height, SC:O(n^(h/2))
+# top-down dp, TC:O(n*sqrt(n)), SC:O(N)
 def numSquares2(n: int) -> int:
+    cand = [i**2 for i in range(int(n**0.5), 0, -1)]
+    @functools.lru_cache(None)
+    def helper(n):
+        if n < 0:
+            return float('inf')
+        if n == 0:
+            return 0
+        res = float('inf')
+        for c in cand:
+            res = min(res, helper(n-c) + 1)
+        return res
+
+    return helper(n)
+
+# greedy + bfs, TC:O(n^(h/2)) tree_node * tree_height, SC:O(n^(h/2))
+def numSquares3(n: int) -> int:
     options = [i**2 for i in range(int(n**0.5),0,-1)]
     # bfs to find the shortest path
     queue = collections.deque([(n, 0)])
@@ -27,3 +43,6 @@ def numSquares2(n: int) -> int:
             if rem-node == 0:
                 return step + 1
             queue.append((rem-node, step+1))
+
+
+
