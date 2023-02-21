b80ad930827b67b772dadd06fc337fed69383b6a
YCC <ychanc2104@gmail.com>
2023-02-21 21:53:31 +0800

    add a solution

--- Single Element in a Sorted Array.py
+++ Single Element in a Sorted Array.py
@@ -19,3 +19,24 @@ def singleNonDuplicate(nums: List[int]) -> int:
         else:
             return nums[mid]  # must exist
     return nums[L]
+
+
+# binary search, TC:O(logN), SC:O(1)
+def singleNonDuplicate2(nums: List[int]) -> int:
+    L, R = 0, len(nums)-1
+    while True:
+        mid = (L + R)//2
+        if mid>0 and nums[mid] == nums[mid-1]:
+            if mid % 2: # odd
+                # go right
+                L = mid + 1
+            else:
+                R = mid - 1
+        elif mid<len(nums)-1 and nums[mid] == nums[mid+1]:
+            if mid % 2:
+                # go left
+                R = mid - 1
+            else:
+                L = mid + 1
+        else:
+            return nums[mid]
