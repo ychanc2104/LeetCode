184b26dbc81a96808d592a02e1808bba4d0b37d0
YCC <ychanc2104@gmail.com>
2023-03-13 15:09:21 +0800

    add iterative solutions

--- Symmetric Tree.py
+++ Symmetric Tree.py
@@ -1,5 +1,6 @@
 # https://leetcode.com/problems/symmetric-tree/
 
+import collections
 
 # Definition for a binary tree node.
 # class TreeNode:
@@ -22,3 +23,42 @@ def isSymmetric(root) -> bool:
         else:
             return False
     return sameTree(root.left, root.right)
+
+# iterative dfs, TC:O(n), SC:O(logn) or O(n) (linear tree)
+def isSymmetric2(root) -> bool:
+    p, q = root.left, root.right
+    stack1 = []
+    stack2 = []
+    while p or q or stack1 or stack2:
+        while p or q:
+            if (not p and q) or (p and not q) or (p.val != q.val):
+                return False
+            stack1.append(p)
+            p = p.left
+            stack2.append(q)
+            q = q.right
+
+        p = stack1.pop()
+        q = stack2.pop()
+        p = p.right
+        q = q.left
+    return True
+
+
+
+# iterative bfs, level-order traversal, TC:O(n), SC:O(logn) or O(n) (linear tree)
+def isSymmetric3(root) -> bool:
+    queue = collections.deque([root.left, root.right])
+    while queue:
+        p = queue.popleft()
+        q = queue.popleft()
+        if not p and not q:
+            continue
+        if (not p and q) or (p and not q) or (p.val != q.val):
+            return False
+        queue.append(p.left)
+        queue.append(q.right)
+        queue.append(p.right)
+        queue.append(q.left)
+
+    return True
