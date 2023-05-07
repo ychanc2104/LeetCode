6f13f9b00bbc002c1c5d4ffe0b8149c04a18296d
YCC <ychanc2104@gmail.com>
2023-05-07 15:58:54 +0800

    add a solution

--- Search a 2D Matrix.py
+++ Search a 2D Matrix.py
@@ -1,32 +1,5 @@
 # https://leetcode.com/problems/search-a-2d-matrix/
-
-
-# binary search, TC:O(N+logM), N is number of rows, M is number of columns, SC:O(1)
-def searchMatrix(matrix, target: int) -> bool:
-    # find target row and do binary search
-    i_row = 0
-    nrow = len(matrix)
-    #ncol = len(matrix[0])
-    # TC:O(N)
-    for i in range(1, nrow):
-        if matrix[i_row][0] == target or matrix[i][0] == target:
-            return True
-        if matrix[i_row][0] < target <= matrix[i][0]:
-            break
-        else:
-            i_row += 1
-    # print(i_row)
-    L, R = 0, len(matrix[i_row]) - 1
-    while L <= R:
-        mid = L + (R - L) // 2
-        if matrix[i_row][mid] > target:
-            # move R to mid
-            R = mid - 1
-        elif matrix[i_row][mid] < target:
-            L = mid + 1
-        else:
-            return True
-    return False
+# https://leetcode.com/problems/search-a-2d-matrix/solutions/487459/three-c-solutions-o-m-n-o-m-n-o-log-m-n/
 
 # binary search, TC:O(logNM), N is number of rows, M is number of columns, SC:O(1)
 def searchMatrix2(matrix, target: int) -> bool:
@@ -45,4 +18,20 @@ def searchMatrix2(matrix, target: int) -> bool:
             L = mid + 1
         else:
             return True
+    return False
+
+
+# saddleback search, TC:O(N+M), SC:O(1)
+def searchMatrix2(matrix: List[List[int]], target: int) -> bool:
+    # saddleback search
+    m, n = len(matrix), len(matrix[0])
+    r, c = 0, n-1
+    while r < m and c >= 0:
+        cell = matrix[r][c]
+        if cell == target:
+            return True
+        elif cell < target:
+            r += 1
+        elif cell > target:
+            c -= 1
     return False
