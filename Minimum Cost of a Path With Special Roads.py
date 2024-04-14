8200c1f7ca4d556c3737117a4edb196e0933dc02
ian <ian.chang@robinstech.com.tw>
2024-04-14 18:19:34 +0800

    refine code

--- Minimum Cost of a Path With Special Roads.py
+++ Minimum Cost of a Path With Special Roads.py
@@ -7,23 +7,22 @@ import heapq, collections
 
 # Dijkstra, TC:O(), SC:O()
 def minimumCost(start: List[int], target: List[int], specialRoads: List[List[int]]) -> int:
-    # from i,j to end of roads or i,j to target
+    # dp(i,j): min cost from start to (i,j)
+    def cal_dist(x1,y1,x2,y2):
+        return abs(x1-x2) + abs(y1-y2)
 
-    specialRoads = [[x1, y1, x2, y2, c] for x1, y1, x2, y2, c in specialRoads if c < abs(x1 - x2) + abs(y1 - y2)]
-    heaps = [(0, start[0], start[1])]
-    dp = collections.defaultdict(lambda: float('inf'))
-    dp[(start[0], start[1])] = 0  # from start to (i,j)
-    while heaps:
-        total, x, y = heapq.heappop(heaps)  # total: cost to (x,y)
-        for x1, y1, x2, y2, c in specialRoads:
-            cost_to_end = abs(x - x1) + abs(y - y1) + c
-            # end_to_target = abs(x2 - target[0]) + abs(y2 - target[1])
-            if dp[(x2, y2)] > total + cost_to_end:
-                heapq.heappush(heaps, (total + cost_to_end, x2, y2))
-                dp[(x2, y2)] = total + cost_to_end
-    # print(dp)
-    res = min(dp[(target[0], target[1])], abs(start[0] - target[0]) + abs(start[1] - target[1]))
-    for x1, y1, x2, y2, c in specialRoads:
-        res = min(res, dp[(x2, y2)] + abs(x2 - target[0]) + abs(y2 - target[1]))
+    specialRoads = [road for road in specialRoads if road[-1] < cal_dist(*road[:-1])]
+    dp = collections.defaultdict(lambda : float("inf"))
+    heap = [(0, *start)]
+    res = cal_dist(*start, *target)
+    while heap:
+        d, x, y = heapq.heappop(heap)
+        res = min(res, dp[(x, y)] + cal_dist(x,y,*target))
+
+        for x1,y1,x2,y2,cost in specialRoads:
+            new_dist = d + cal_dist(x,y,x1,y1) + cost
+            if dp[(x2, y2)] > new_dist:
+                dp[(x2, y2)] = new_dist
+                heapq.heappush(heap, (new_dist, x2, y2))
 
     return res
